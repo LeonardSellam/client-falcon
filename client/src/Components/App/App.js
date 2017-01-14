95d7773ab5fe24d6a9c81e802ec2f19d403e0b9c
sachito <sacha.bellaiche@gmail.com>
2017-01-14 15:21:14 +0100

    delete useless stuff

--- client/src/Components/App/App.js
+++ client/src/Components/App/App.js
@@ -4,27 +4,21 @@ import ProgressBar from 'react-toolbox/lib/progress_bar';
 
 
 class App extends Component {
-
-  loadFlask() {
-    fetch('http://127.0.0.1:5000/api')
-    .then(result=> {
-      console.log(result.text)
-    });
-  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started with this app, edit <code>src/App.js</code> and save to reload.
-        </p>
         <Button label='Bookmark' raised primary />
         <ProgressBar type="linear" mode="determinate" value={83} buffer={90} />
       </div>
     );
   }
 }
+
+
+
+
 
 export default App;
